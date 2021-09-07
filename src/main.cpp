ac168d15146165066e76ae9ae06cce9936e22a6b
Sergei <delaug@bk.ru>
2021-09-07 23:35:12 +0300

    Deep sleep mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,38 @@
 #include "LiteHTTPClient.h"
 #include "LiteBME280.h"
 
+#define uS_TO_S_FACTOR 1000000
+#define TIME_TO_SLEEP  120
+
 LiteWiFi wifi = LiteWiFi();
 LiteHTTPClient client = LiteHTTPClient();
 LiteBME280 bme = LiteBME280();
 
-int dt = 0;             // delta time
-int sendInterval = 120; // seconds
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  delay(1000);
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
 
   Serial.println("Run ESP32...\n");
   wifi.init("SSID", "PASS");
 
   // BME280 sensor  
   bme.init();
+
+  if(wifi.checkConnection()) {
+    // send data on server
+    client.send("http://192.168.100.100/sensor/","token=TOKEN&"+bme.getHTTPData());      
+
+    // deep sleep
+    Serial.println("Going to sleep now");
+    delay(1000);
+    esp_deep_sleep_start();
+  }  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:  
-  if(wifi.checkConnection()) {
-    // send data on server
-    if(dt >= sendInterval) {
-      client.send("http://192.168.100.100/sensor/","token=TOKEN&"+bme.getHTTPData());
-
-      dt = 0; // clear dt
-    }
-  }
-
-  dt++;
-  delay(1000);
 }
